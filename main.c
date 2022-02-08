884bff29ce9790f98853f26cdf4b09c6767abdcc
Luis <l-b-n.02@gmx.de>
2022-02-08 10:17:49 +0100

    added shell functions

--- main.c
+++ main.c
@@ -5,3 +5,19 @@ int main() {
 
     return 0;
 }
+
+int initialize_board() {
+    // get empty board
+    return 0;
+}
+
+int make_move() {
+    // print board
+    // await input
+    // return board 
+    return 0;
+}
+
+void print_board() {
+    // print board
+}
