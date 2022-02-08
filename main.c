97b4234f0b5c5f02f58138a133c4fc246c8d504e
Luis <l-b-n.02@gmx.de>
2022-02-08 18:14:32 +0100

    main

--- main.c
+++ main.c
@@ -2,38 +2,74 @@
 #include <stdlib.h>
 
 
-int make_move() {
-    // print board
-    // await input
-    // return board
-    return 0;
-}
-
-void print_board(int board) {
-    
+void print_board(char board[3][3]) {
     for (int i=0; i<3; i++) {
         for (int j=0; j<3; j++) {
-            printf("%d ", *board[i][j]);
+            printf("%c ", board[i][j]);
         }
         printf("\n");
     }
     //prints board
 }
 
-void run_game() {
-
-    int board[3][3];
-    int (*boardp)[][];
-    for (int i=0; i<3; i++) {
-        for (int j; j<3; j++) {
-            board[i][j] = '_';
-        }
+char check_for_win(char board[3][3]) { //returns character of player who won or _
+    if (board[0][0] == board[0][1] && board[0][0] == board[0][2]) {
+        return board[0][0];
     }
-    
-    print_board(boardp);
+    if (board[1][0] == board[1][1] && board[1][0] == board[1][2]) {
+        return board[1][0];
+    }
+    if (board[2][0] == board[2][1] && board[2][0] == board[2][2]) {
+        return board[2][0];
+    }
+    if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+        return board[0][0];
+    }
+    if (board[2][0] == board[1][1] && board[2][0] == board[0][2]) {
+        return board[2][0];
+    }
+    if (board[0][0] == board[1][0] && board[0][0] == board[2][0]) {
+        return board[0][0];
+    }
+    if (board[0][1] == board[1][1] && board[0][1] == board[2][1]) {
+        return board[0][1];
+    }
+    if (board[0][2] == board[1][2] && board[0][2] == board[2][2]) {
+        return board[0][2];
+    }
+    return '_';
+}
+
+void check_for_input(char board[3][3], int turnindex) {
+    char playerchar;
+    if (turnindex % 2 == 0)  {
+        playerchar = 'X';
+    }
+    else {
+        playerchar = 'O';
+    }
+    printf("Please enter index to place your ");
+    printf("%c ", playerchar);
+    printf("\n");
+    int i = (int) getchar();
+    int rowindex = i % 3;
+    int columnindex = (i-(i%3)) / 3;
+    if (board[rowindex][columnindex] == '_') {
+        board[rowindex][columnindex] = playerchar;
+    } 
+    else {
+        printf("Not possible!\n");
+        check_for_input(board, turnindex);
+    }
+}
+
+void run_game() {
+    char board[3][3]= {{'_','_','_'},{'_','_','_'},{'_','_','_'}};
+    print_board(board);
 
     int turnindex = 0;
-    while (check_for_win(board) == "_") {
+
+    while (check_for_win(board) == '_') {
         check_for_input(board, turnindex);
         turnindex += 1;
     }
@@ -47,32 +83,15 @@ void run_game() {
         playerchar = 'O';
     }
     printf("CONGRATULATIONS! Player ");
-    printf(playerchar);
+    printf("%c ", playerchar);
     printf(" won.\n");
 }
 
-void check_for_input(int board, int turnindex) {
-    char playerchar;
-    if (turnindex % 2 == 0)  {
-        playerchar = 'X';
-    }
-    else {
-        playerchar = 'O';
-    }
-    printf("Please enter index to place your ");
-    printf(playerchar);
-    printf("\n");
-    int i = (int) getchar();
-    int rowindex = i % 3;
-    int columnindex = (i-(i%3)) / 3;
-    if (board[rowindex][columnindex] == '_') {
-        board[rowindex][columnindex] = playerchar;
-    } 
-    else {
-        printf('Not possible!\n');
-        check_for_input(board, turnindex);
-    }
-}
+
+
+
+
+
 
 int main() {   
 
@@ -80,32 +99,4 @@ int main() {
     run_game();
 
     return 0;
-}
-
-char check_for_win(int board) { //returns character of player who won or _
-    if (board[0][0] = board[0][1]) and (board[0][0] = board[0][2]) {
-        return board[0][0];
-    }
-    if (board[1][0] = board[1][1]) and (board[1][0] = board[1][2]) {
-        return board[1][0];
-    }
-    if (board[2][0] = board[2][1]) and (board[2][0] = board[2][2]) {
-        return board[2][0];
-    }
-    if (board[0][0] = board[1][1]) and (board[0][0] = board[2][2]) {
-        return board[0][0];
-    }
-    if (board[2][0] = board[1][1]) and (board[2][0] = board[0][2]) {
-        return board[2][0];
-    }
-    if (board[0][0] = board[1][0]) and (board[0][0] = board[2][0]) {
-        return board[0][0];
-    }
-    if (board[0][1] = board[1][1]) and (board[0][1] = board[2][1]) {
-        return board[0][1];
-    }
-    if (board[0][2] = board[1][2]) and (board[0][2] = board[2][2]) {
-        return board[0][2];
-    }
-    return '_';
 }
